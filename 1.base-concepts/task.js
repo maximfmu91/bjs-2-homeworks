9fc78a315e3d2a68c4b7787f06e88933b841527f
Максим Филимонов <fmu91@yandex.ru>
2023-04-06 19:33:23 +0700

    Решение первого задания

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,51 @@
-"use strict"
+"use strict";
 function solveEquation(a, b, c) {
   let arr = [];
+  let d = b**2 - 4 * a * c;
+  arr = d;
   
+  if (d < 0) {
+    arr = [];
+}
+  let str = [];
+if (d === 0) {
+  str.push(-b/(2*a));
+  arr = str;
+} else if (d > 0) {
+  str.push((-b + Math.sqrt(d)) / (2*a));
+  str.push((-b - Math.sqrt(d)) / (2*a));
+  arr = str;
+}
+
   return arr;
 }
 
+
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
+
+  if (typeof percent === 'string') {
+    percent = +percent;
+  }
+// if (typeof contribution === 'string') {
+//     contribution = +contribution;
+//   }
+//   if (typeof amount === 'string') {
+//     amount = +amount;
+//   }
+//   if (typeof countMonths === 'string') {
+//     countMonths = +countMonths;
+//   }
+
+  if (isNaN(percent) ||
+      isNaN(contribution) ||
+      isNaN(amount) ||
+      isNaN(countMonths)) {
+        return false;
+      }
+  amount = amount - contribution;
+  let P = (percent / 100) / 12; 
+  let payPerMonth = amount * (P + (P / (((1 + P)**countMonths) - 1)));
+  let totalResult = payPerMonth * countMonths;
   
+  return (+totalResult.toFixed(2));
 }
