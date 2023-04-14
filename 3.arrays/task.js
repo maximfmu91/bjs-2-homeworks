2217adf4c8a2e5dadfd2ec4c2a9c168270f65f0d
Максим Филимонов <fmu91@yandex.ru>
2023-04-14 14:06:48 +0700

    Решение домашнего задания

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,13 @@
 function compareArrays(arr1, arr2) {
-  
-}
+    let compareNums = arr1.length === arr2.length && arr1.every((num, index) => num === arr2[index]);
+  return compareNums;
+  }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+  let peoplesFilter = users.filter(user => user.gender === gender);
+  let sumAges = peoplesFilter.map(user => user.age).reduce((acc, userAge) => acc + userAge, 0);
+    if(isNaN((sumAges / peoplesFilter.length))) {
+    return 0;
+  }
+  return (sumAges / peoplesFilter.length);
 }
